c4cfed37b7f70c07619e85d45a5fabfe65f715c2
Viacheslav Blazhkov <blazkov253@gmail.com>
2026-06-01 18:55:54 +0300

    [FIX] Fixed symbols in product descriptions

--- src/pages/ProductPage/Sections/DetailTabs/Description.jsx
+++ src/pages/ProductPage/Sections/DetailTabs/Description.jsx
@@ -1,7 +1,20 @@
 const Description = ({product}) => {
+	const normalizeText = (value) => {
+		let s = String(value ?? "");
+		s = s.replace(/\r\n/g, "\n");
+		s = s.replace(/\\r\\n/g, "\n");
+		s = s.replace(/\\n/g, "\n");
+		s = s.replace(/\\r/g, "\n");
+		s = s.replace(/\\t/g, "\t");
+		s = s.replace(/\\u([0-9a-fA-F]{4})/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)));
+		s = s.replace(/\\x([0-9a-fA-F]{2})/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)));
+		s = s.replace(/\\\\/g, "\\");
+		return s;
+	};
+	const text = normalizeText(product?.description);
 	return (
 		<div className="whitespace-pre-line">
-			{product.description}
+			{text}
 		</div>
 	);
 }
